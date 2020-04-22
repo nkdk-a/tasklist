b570219205d968ae264ec4fd90f710641aaa8825
nkdk-a <airi.nakadake@artemis-tech.jp>
2020-04-22 09:46:00 +0000

    init Laravel Project

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('app')
 
 @section('content')
 <!-- ここにページ毎のコンテンツを書く -->
@@ -13,7 +13,7 @@
             <td>{{ $task->content }}</td>
         </tr>
     </table>
-    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
+    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
